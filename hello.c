1d5b9efe39edec824605a849326113aca45ceaea
boeschj <boeschj@users.noreply.github.com>
2020-01-16 19:13:18 -0600

    Update hello.c
    Added forward slash instead of backslash

--- hello.c
+++ hello.c
@@ -4,8 +4,8 @@ int main (void){
 
 printf("Taylor Woodsmall\n");
 printf("Women's and Gender Studies\n");
-printf("Jordan Boesch/n");
-printf("Computer Science/n");
+printf("Jordan Boesch\n");
+printf("Computer Science\n");
 
 return 0;
 }
